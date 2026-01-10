d97522f72551e08b52c876c56eb602aab8332b0c
lynn <lynnplus90@gmail.com>
2026-01-11 00:23:46 +0800

    commit

--- cmake/ModuleHelpers.cmake
+++ cmake/ModuleHelpers.cmake
@@ -3,7 +3,12 @@
 add_library(bix_internal_link_pool INTERFACE)
 
 function(bix_module_setup NAME)
-    target_link_libraries(${NAME} PRIVATE bix_build_config)
-    target_link_libraries(${NAME} PUBLIC ${ARGN})
+    target_link_libraries(${NAME} PRIVATE bix_build_config PUBLIC ${ARGN})
     target_link_libraries(bix_internal_link_pool INTERFACE ${ARGN})
+
+    set_target_properties(${NAME} PROPERTIES
+            POSITION_INDEPENDENT_CODE ON
+            CXX_VISIBILITY_PRESET hidden
+            VISIBILITY_INLINES_HIDDEN ON
+    )
 endfunction()
